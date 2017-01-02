ef6533394771ea7831c07df10d24433b69bc55d2
zhangrunqin <dsj@minzhulou.com>
2017-01-02 11:13:12 +0800

    first demo

---
+++ www/app.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+from __future__ import unicode_literals
+from flask import Flask
+from flask import (render_template, g, session, redirect, url_for, request, flash)
+from flask_bootstrap import Bootstrap
+
+
+
---
+++ www/utils.py
@@ -0,0 +1,7 @@
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+import datetime
+
+
+def utc_time():
+    return datetime.datetime.utcnow()
